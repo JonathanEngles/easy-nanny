64607b99ac569731bbb0ac482b1e7afce52fbb22
JonathanEngles <jonathan.engles74@gmail.com>
2023-04-06 08:45:35 +0200

    :sparkles: add function modify user

--- app/routers/nannyRouter.js
+++ app/routers/nannyRouter.js
@@ -6,14 +6,29 @@ const { catchErrors } = require('../middlewares/handlerError');
 const router = express.Router();
 
 /**
- * comments
+ * routes for the signup form in GET and create user in POST
  */
 // router.get('/signup', nannyController.signup.bind(nannyController));
 router.post('/signup', nannyController.register.bind(nannyController));
 
-
+/**
+ * routes for the login form in GET and open session in POST
+ */
 //router.get('/login', parentController.loginForm.bind(nannyController));
 router.post('/login',nannyController.login.bind(nannyController));
 
+/**
+ * routes for the profil in GET/PATCH/DELETE
+ */
+
+router.get('/profile', nannyController.getProfile.bind(nannyController));
+// router.patch('/profile', nannyController.modifyProfile.bind(nannyController));
+// router.delete('/profile', nannyController.deleteProfile.bind(nannyController));
+
+
+/**
+ * route for disconnect to the session 
+ */
+router.get('/logout', nannyController.logout.bind(nannyController));
 
 module.exports = router;
